dc03f0177e708f2d994d257dddb1dd5fed24d29a
dmitrogov <drogovborn@gmail.com>
2025-04-23 02:11:27 +0500

    Initial commit

---
+++ Tyuiu.RogovDY.Lab1.Review.V8.Test/DataServiceTest.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using Tyuiu.RogovDY.Lab1.Review.V8.Lib;
+
+namespace Tyuiu.RogovDY.Lab1.Review.V8.Test
+{
+    [TestClass]
+    public class DataServiceTest
+    {
+        [TestMethod]
+        public void CheckCalc()
+        {
+            double x = 0.9;
+            double y = 0.6;
+
+            DataService ds = new DataService();
+
+            double result = ds.Calc(x, y);
+
+            double true_val = 0.762;
+
+            Assert.AreEqual(true_val, result);
+        }
+    }
+}
---
+++ Tyuiu.RogovDY.Lab1.TaskReview/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Tyuiu.RogovDY.Lab1.Review.V8.Lib;
+
+namespace Tyuiu.RogovDY.Lab1.TaskReview
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            DataService ds = new DataService();
+            Console.Title = "Лабораторная работа #1 | Выполнил: Рогов Д.Ю. | НТм-23-1 ";
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* Лабораторная работа #1                                                  *");
+            Console.WriteLine("* Тема: Базовые навыки создания консольных приложений по методологии TDD. *");
+            Console.WriteLine("* Вариант #8                                                              *");
+            Console.WriteLine("* Выполнил: Рогов Дмитрий Юрьевич | НТм-23-1                              *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* УСЛОВИЕ:                                                                *");
+            Console.WriteLine("* Написать программу, которая вычисляет математическое выражение по       *");
+            Console.WriteLine("* исходным значениям данных, вводимых пользователем.                      *");
+            Console.WriteLine("* Ответ округлить до трех знаков после запятой.                           *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* Введите значение X:                                                     *");
+            double x = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("* Введите значение Y:                                                     *");
+            double y = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine(ds.Calc(x, y));
+            Console.WriteLine("***************************************************************************");
+            Console.ReadKey();
+        }
+    }
+}
